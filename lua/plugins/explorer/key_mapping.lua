a0a37aaa81d973e2e914597c0006a863f5be02dc
Tuan Bui <tuanba1@gmail.com>
2023-07-11 13:52:56 +0700

    feat: custom for neotree

---
+++ lua/plugins/explorer/key_mapping.lua
@@ -0,0 +1,37 @@
+local M = {}
+
+-- Default mappings. Feel free to modify or remove as you wish.
+-- https://github.com/nvim-neo-tree/neo-tree.nvim/blob/v2.x/lua/neo-tree/defaults.lua
+--
+
+M.keymap = {
+  ["o"] = {
+    "toggle_node",
+    nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+  },
+  ["<Tab>"] = { "toggle_preview", config = { use_float = false } },
+
+  -- copy relative path
+  ["Y"] = function(state)
+    local node = state.tree:get_node()
+    -- local content = node.path
+    -- relative
+    local content = node.path:gsub(state.path, ""):sub(2)
+    vim.fn.setreg('"', content)
+    vim.fn.setreg("1", content)
+    vim.fn.setreg("+", content)
+  end,
+
+  -- copy full (absolute path)
+  ["gy"] = function(state)
+    local node = state.tree:get_node()
+    local content = node.path
+    -- relative
+    -- local content = node.path:gsub(state.path, ""):sub(2)
+    vim.fn.setreg('"', content)
+    vim.fn.setreg("1", content)
+    vim.fn.setreg("+", content)
+  end,
+}
+
+return M
