076b60052a6f4c27452a59799fa384dcfe2b7ebe
Tuan Bui <tuanba1@gmail.com>
2023-07-12 13:25:49 +0700

    feat(github): add integration with github via Octo.nvim

---
+++ lua/plugins/github/init.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup({ default_remote = { "origin", "upstream" } })
+    end,
+  },
+}
+
+-- TODO: Not complete yet
