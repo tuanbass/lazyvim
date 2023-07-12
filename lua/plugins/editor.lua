82555d5b54eadcf8f689369dc8f273c030edb7ec
Tuan Bui <tuanba1@gmail.com>
2023-07-12 13:30:46 +0700

    feat(editor): Add Command to close multiple buffer

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -16,6 +16,7 @@ return {
       -- other default shortcut can be found in $HOME/.local/share/nvim_plugins/lazyvim/lazy/LazyVim/lua/lazyvim/plugins/extras/coding/yanky.lua
     },
   },
+  -- enhanced subtitude
   {
     "gbprod/substitute.nvim",
     config = function(opts)
@@ -42,5 +43,84 @@ return {
     end,
     enabled = false,
   },
-  -- enhanced subtitude
+
+  -- command help to close multiple buffers
+  {
+    "kazhala/close-buffers.nvim",
+    config = function(plugin)
+      local opts = plugin.opts
+      require("close_buffers").setup(opts)
+      local cmds = plugin.cmds or {}
+      for _, value in pairs(cmds) do
+        local cmdName = value[1]
+        local cmdFunc = value[2]
+        vim.api.nvim_create_user_command(cmdName, cmdFunc, {})
+      end
+    end,
+
+    opts = {
+      preserve_window_layout = { "this", "other", "all" },
+      next_buffer_cmd = function(windows)
+        require("bufferline").cycle(1)
+        local bufnr = vim.api.nvim_get_current_buf()
+
+        for _, window in ipairs(windows) do
+          vim.api.nvim_win_set_buf(window, bufnr)
+        end
+      end,
+    },
+
+    -- define some custom command, and register them in the config function
+    cmds = {
+      -- command! BufferKillForce lua require('lvim.core.bufferline').buf_kill('bd', nil, true)
+      {
+        "BD1this",
+        function()
+          vim.cmd("BDelete this") -- BDelete this
+          -- vim.cmd(".,$-bdelete")
+          -- require('close_buffers').delete({ type = 'other' })
+        end,
+      },
+      {
+        "BDReopen",
+        function()
+          vim.cmd("BDelete this") -- BDelete this
+          vim.cmd("edit #") -- reopen last file
+          -- vim.cmd(".,$-bdelete")
+          -- require('close_buffers').delete({ type = 'other' })
+        end,
+      },
+      {
+        "BDthisForce",
+        function()
+          vim.cmd("BDelete! this")
+          -- vim.cmd(".,$-bdelete")
+          -- require('close_buffers').delete({ type = 'other' })
+        end,
+      },
+      {
+        "BD2Other",
+        function()
+          -- vim.cmd("%bdelete")
+          vim.cmd("BufferLineCloseLeft")
+          vim.cmd("BufferLineCloseRight")
+        end,
+      },
+      {
+        "BD3All",
+        function()
+          -- vim.cmd("%bdelete")
+          vim.cmd("BD2Other")
+          vim.cmd("BD1this")
+        end,
+      },
+
+      -- {
+      --   "ToggleScratchPad",
+      --   function()
+      --     print("type dsp in normal mode to toggle Scratpad")
+      --   end,
+      -- },
+    },
+  },
 }
