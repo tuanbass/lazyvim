453c4c27a26735f4241046d7cf52393c1d15f7d2
Tuan Bui <tuanba1@gmail.com>
2023-07-12 13:45:22 +0700

    fix(view): invalid color for current line
    bug in vimade

--- lua/plugins/view.lua
+++ lua/plugins/view.lua
@@ -1,6 +1,28 @@
 return {
   {
     "TaDaa/vimade",
+    config = function(plugin)
+      vim.g.vimade = {
+        basegroups = {
+          -- "Folded",
+          -- "Search",
+          -- "SignColumn",
+          -- -- "LineNr",
+          -- -- "CursorLine",
+          -- -- "CursorLineNr",
+          -- "DiffAdd",
+          -- "DiffChange",
+          -- "DiffDelete",
+          -- "DiffText",
+          -- "FoldColumn",
+          -- "Whitespace",
+          -- "NonText",
+          -- "SpecialKey",
+          -- "Conceal",
+          -- "EndOfBuffer",
+        },
+      }
+    end,
   },
   {
     "folke/zen-mode.nvim",
