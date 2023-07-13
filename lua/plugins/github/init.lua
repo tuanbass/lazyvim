e8e67ac06b32afd0342be2e52607415292ff7d18
Tuan Bui <tuanba1@gmail.com>
2023-07-13 15:52:12 +0700

    feat(github): more integration with octo

--- lua/plugins/github/init.lua
+++ lua/plugins/github/init.lua
@@ -1,3 +1,18 @@
+octo_menu = function()
+  --
+  local buf_num = vim.fn.bufnr()
+  local handler
+  if octo_buffers[buf_num] then
+    local picker = require("plugins.github.picker")
+    -- print("in a octo buffer")
+    handler = picker.show
+  else
+    -- print("not in a octo buffer")
+    handler = require("octo.commands").actions
+  end
+  handler()
+end
+
 return {
   {
     "pwntester/octo.nvim",
@@ -6,9 +21,32 @@ return {
       "nvim-telescope/telescope.nvim",
       "nvim-tree/nvim-web-devicons",
     },
-    config = function()
+    config = function(plugin)
       require("octo").setup({ default_remote = { "origin", "upstream" } })
+      require("utils.cmd").createCommand(plugin.cmds or {})
+      require("utils.cmd").createAutoCommand(plugin.auto_cmds or {})
     end,
+
+    cmds = {
+      {
+        "GithubMenu",
+        function()
+          -- local github = require("plugins.github")
+          octo_menu()
+        end,
+      },
+    },
+
+    auto_cmds = {
+      {
+        "FileType",
+        "octo",
+        function()
+          -- vim.cmd([[ map <silent> <buffer> g? :GithubMenu<CR> ]])
+          require("utils.keys").safe_map("n", "g?", "<cmd>GithubMenu<cr>", { buffer = vim.fn.bufnr(), silent = true })
+        end,
+      },
+    },
   },
 }
 
--- lua/utils/cmd.lua
+++ lua/utils/cmd.lua
@@ -1,13 +1,43 @@
 local M = {}
+local lazy_command_group = "LazyCustomAu"
+vim.api.nvim_create_augroup(lazy_command_group, { clear = false })
 do
   -- Private --------
   -- publics
+
+  -- Batch create Cmd
+  -- params: list of Command to create
   M.createCommand = function(commandList)
     for _, value in pairs(commandList) do
       local cmdName = value[1]
       local cmdFunc = value[2]
       local param = value[3] or {}
       vim.api.nvim_create_user_command(cmdName, cmdFunc, param)
+    end
+  end
+
+  -- Batch create AutoCmd
+  -- params: list of AutoCmd to create
+  M.createAutoCommand = function(commandList)
+    for _, value in pairs(commandList) do
+      local event = value[1]
+      local pattern = value[2]
+      local action = value[3]
+      local opts = value[4] or {}
+
+      local o = {
+        pattern = pattern,
+        group = lazy_command_group,
+      }
+      opts = vim.tbl_extend("force", o, opts)
+
+      if type(action) == "function" then
+        opts.callback = action
+      else
+        opts.command = action
+      end
+
+      vim.api.nvim_create_autocmd(event, opts)
     end
   end
 end
