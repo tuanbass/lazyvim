2d87b86393a41eddd3a430b53a37cc7ec890ca8c
Tuan Bui <tuanba1@gmail.com>
2023-07-13 11:19:40 +0700

    feat(navigation): add some navigation util

---
+++ lua/plugins/navigation.lua
@@ -0,0 +1,21 @@
+return {
+
+  { -- j/k to escape
+    "max397574/better-escape.nvim",
+    cond = function()
+      return vim.g.vscode ~= 1
+    end,
+    config = function()
+      require("better_escape").setup({
+        mapping = { "jk", "jj", "kj", "kk" }, -- a table with mappings to use
+        keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+      })
+    end,
+  },
+}
+
+-- There are some preconfigured shortcuts that are very handy:
+--     { "za", desc = "Toggle fold" },
+--     { "<leader>ss", desc = "Navigate all function" },
+--     { "gd", desc = "Go to definition" },
+--
