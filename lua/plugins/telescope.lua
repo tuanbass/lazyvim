1509baa67a144d9ba1eb10b710df36f7061a40d8
Tuan Bui <tuanba1@gmail.com>
2023-07-13 11:19:16 +0700

    fix(telescope): change some telescope settings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,16 @@
 return {
+  {
+    "nvim-telescope/telescope.nvim",
+  },
+  {
+    -- force all select action in nvim to use telescope
+    "nvim-telescope/telescope-ui-select.nvim",
+    -- cmd = "Telescope ",
+    dependencies = "telescope.nvim",
+    config = function()
+      require("telescope").load_extension("ui-select")
+    end,
+  },
   { -- fuzzy search
     -- NOTES: it works only for grep_string and find_files, anf not for live_grep (as live_grep use ag directly, not use telescope sorter )
     -- hint: live_grep a word, then <C-space> to switch to grep_string, then we can use fzf sorter
@@ -8,9 +20,9 @@ return {
     config = function()
       require("telescope").load_extension("fzf")
     end,
-    lazy = false,
     build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
     -- TODO: Currently, sometime you need to go to /.local/share/nvim/lazy/telescope-fzf-native.nvim and manual run make.
     -- Need to find a way to run make automatically. Dont know why build command not work}
+    keys = { { "<leader>se", "<cmd>Telescope sessions_picker<cr>", desc = "sEssion" } },
   },
 }
