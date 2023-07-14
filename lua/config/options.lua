a96c56a19fadb71a442d4c1d05a8ea893e894e46
Tuan Bui <tuanba1@gmail.com>
2023-07-14 14:46:37 +0700

    fix(fold): avoid autoclose the fold when back from insert to normal mode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,10 +5,11 @@
 local opt = vim.opt
 
 opt.foldcolumn = "1" -- '0' is not bad
-opt.foldlevel = 2 -- Using ufo provider need a large value, feel free to decrease the value
-opt.foldlevelstart = 2
+opt.foldlevel = 99 -- Using ufo plugin provider need a large value, feel free to decrease the value
+opt.foldlevelstart = 99
 opt.foldenable = true
 
+vim.g.ufo_fold_level_start = 2 -- start fold level when open new file. Not work yet.
 opt.formatoptions = "jcrql" -- disable comment continuation with "o"
 
 opt.winminwidth = 1 -- required for TaDaa/vimade work properly
--- lua/plugins/fold.lua
+++ lua/plugins/fold.lua
@@ -1,9 +1,22 @@
+function changeFoldLevel(level)
+  if level < 0 then
+    level = 0
+  end
+
+  vim.w.ufo_fold_level = level
+  print("fold to level:", level)
+  require("ufo").closeFoldsWith(level)
+end
+
 return {
   {
     "kevinhwang91/nvim-ufo",
     dependencies = "kevinhwang91/promise-async",
+    lazy = false,
     config = function(plugin)
+      -- print("config ufo")
       require("utils.cmd").createCommand(plugin.cmds or {})
+      require("utils.cmd").createAutoCommand(plugin.auto_cmds)
 
       require("ufo").setup({
         provider_selector = function(bufnr, filetype, buftype)
@@ -29,6 +42,19 @@ return {
         },
       })
     end,
+    auto_cmds = { -- set default foldlevel when open window
+
+      {
+        { "WinNew", "VimEnter" },
+        "*",
+        function()
+          vim.w.ufo_fold_level = vim.g.ufo_fold_level_start or 3
+          -- TODO: still an issue when first time load a window, the start fold level not work
+          -- possibly because the plugin is not loaded yet
+          changeFoldLevel(vim.w.ufo_fold_level)
+        end,
+      },
+    },
 
     cmds = {
       {
@@ -36,33 +62,49 @@ return {
         function(opts)
           local foldlvel = opts.args
           if foldlvel == "" then
-            foldlvel = vim.opt.foldlevelstart.get(vim.opt.foldlevelstart) -- Litle ugly but works
+            foldlvel = vim.g.ufo_fold_level_start - 1
           end
-          vim.cmd("set foldlevel=" .. foldlvel)
-        end,
-        { nargs = "*" },
-      },
-      {
-        "Fold",
-        function(opts)
-          local foldlvel = opts.args
-          if foldlvel == "" then
-            foldlvel = "2"
-          end
-          print("Fold: " .. vim.inspect(foldlvel))
-          vim.cmd("set foldlevelstart=" .. foldlvel)
-          vim.cmd("set foldlevel=" .. foldlvel)
+          changeFoldLevel(tonumber(foldlvel))
         end,
         { nargs = "*" },
       },
     },
 
     keys = {
-      { "z1", "<cmd>FoldL 1<cr>", desc = "FoldL 1" },
-      { "z2", "<cmd>FoldL 2<cr>", desc = "FoldL 2" },
-      { "z3", "<cmd>FoldL 3<cr>", desc = "FoldL 3" },
-      { "z4", "<cmd>FoldL 4<cr>", desc = "FoldL 4" },
-      { "z9", "<cmd>FoldL 9<cr>", desc = "FoldL 9" },
+      { "z0", "<cmd>FoldL<cr>", desc = "FoldL default " },
+      { "z1", "<cmd>FoldL 0<cr>", desc = "FoldL 1" },
+      { "z2", "<cmd>FoldL 1<cr>", desc = "FoldL 2" },
+      { "z3", "<cmd>FoldL 2<cr>", desc = "FoldL 3" },
+      { "z4", "<cmd>FoldL 3<cr>", desc = "FoldL 4" },
+      { "z9", "<cmd>FoldL 8<cr>", desc = "FoldL 9" },
+      {
+        "zM",
+        function()
+          changeFoldLevel(0)
+        end,
+        desc = "fold close all",
+      },
+      {
+        "zR",
+        function()
+          changeFoldLevel(9)
+        end,
+        desc = "fold open all",
+      },
+      {
+        "zm",
+        function()
+          changeFoldLevel(vim.w.ufo_fold_level - 1)
+        end,
+        desc = "fold more",
+      },
+      {
+        "zr",
+        function()
+          changeFoldLevel(vim.w.ufo_fold_level + 1)
+        end,
+        desc = "fold less",
+      },
     },
   },
 }
