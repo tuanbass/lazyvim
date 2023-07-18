4d58d03fe747eda9b25d0ff10faea5e475c8a7db
Tuan Bui <tuanba1@gmail.com>
2023-07-18 15:16:27 +0700

    feat: custom jump for flash

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -1,3 +1,55 @@
+local Flash = require("flash")
+local function format(opts)
+  -- always show first and second label
+  return {
+    { opts.match.label1, "FlashMatch" },
+    { opts.match.label2, "FlashLabel" },
+  }
+end
+
+local jumpToWord = function()
+  --@param opts Flash.Format
+
+  Flash.jump({
+    search = { mode = "search" },
+    label = {
+      after = false,
+      before = { 0, 0 },
+      uppercase = false,
+      format = format,
+    },
+    pattern = [[\<]],
+    action = function(match, state)
+      state:hide()
+      Flash.jump({
+        search = { max_length = 0 },
+        highlight = { matches = false },
+        label = { format = format },
+        matcher = function(win)
+          -- limit matches to the current label
+          return vim.tbl_filter(function(m)
+            return m.label == match.label and m.win == win
+          end, state.results)
+        end,
+        labeler = function(matches)
+          for _, m in ipairs(matches) do
+            m.label = m.label2 -- use the second label
+          end
+        end,
+      })
+    end,
+    labeler = function(matches, state)
+      local labels = state:labels()
+      for m, match in ipairs(matches) do
+        match.label = labels[math.floor((m - 1) / #labels) + 1]
+        -- first and second label
+        match.label1 = match.label
+        match.label2 = labels[(m - 1) % #labels + 1]
+      end
+    end,
+  })
+end
+
 return {
 
   { -- j/k to escape
@@ -11,6 +63,66 @@ return {
         keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
       })
     end,
+  },
+
+  { -- overwrite settings in the Lazy base
+    "folke/flash.nvim",
+    lazy = false,
+    event = "VeryLazy",
+    vscode = true,
+    ---@type Flash.Config
+    opts = {
+      modes = {
+        char = {
+          jump_labels = true,
+          -- label = { exclude = "hjkliardc" },
+          label = { exclude = "" }, -- add more available char for the label
+          -- enabled = false,
+        },
+      },
+    },
+    config = function(p)
+      require("flash").setup(p.opts)
+      --   local Config = require("flash.config")
+      --   local Char = require("flash.plugins.char")
+      --   for _, motion in ipairs({ "f", "t", "F", "T" }) do
+      --     vim.keymap.set({ "n", "x", "o" }, motion, function()
+      --       print("haha")
+      --       require("flash").jump(Config.get({
+      --         mode = "char",
+      --         search = {
+      --           mode = Char.mode(motion),
+      --           label = { "abcd", exclude = "" }, -- add more available char for the label
+      --           max_length = 2,
+      --         },
+      --       }, Char.motions[motion]))
+      --     end)
+      --   end
+    end,
+    -- stylua: ignore
+    keys = {
+
+      { "*", mode = { "n", "x", "o" }, function() require("flash").jump({  pattern = vim.fn.expand("<cword>") }) end, desc = "Flash" }, -- search word under cursor
+      { "<C-G>", mode = { "n", "x", "o" }, jumpToWord, desc = "Flash" },
+      { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      -- { 
+      --   -- Show diagnostics at target, without changing cursor position
+      --   "<leader>cD", mode = { "n" }, function() 
+      --     require("flash").jump({
+      --       action = function(match, state)
+      --         vim.api.nvim_win_call(match.win, function()
+      --           vim.api.nvim_win_set_cursor(match.win, match.pos)
+      --           vim.diagnostic.open_float()
+      --         end)
+      --         state:restore()
+      --       end,
+      --     })
+      -- end, desc = "Dianostics (no curs)" },
+
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
   },
 }
 
