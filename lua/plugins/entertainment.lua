cb6f0b65ba6030895e0b397470435ccf70eb469f
Tuan Bui <tuanba1@gmail.com>
2023-07-19 12:04:22 +0700

    feat: control spotify from vim

---
+++ lua/plugins/entertainment.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "stsewd/spotify.nvim",
+    build = function()
+      vim.cmd([[
+        :UpdateRemotePlugins <cr>
+      ]])
+    end,
+    keys = {
+      { "<leader>en", ":Spotify next<cr>", desc = "SpotNext" },
+      { "<leader>ep", ":Spotify next<cr>", desc = "SpotPrev" },
+      { "<leader>es", ":Spotify play/pause<cr>", desc = "SpotPlay/Pause" },
+      { "<leader>eu", ":Spotify status<cr>", desc = "SpotStatus" },
+      { "<leader>eS", ":Spotify show<cr>", desc = "SpotShow" },
+    },
+  },
+}
