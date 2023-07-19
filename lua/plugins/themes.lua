9063dd1b956d9524a612195b5cc097b271317550
Tuan Bui <tuanba1@gmail.com>
2023-07-19 12:01:31 +0700

    feat: add some themes

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,18 @@
+-- vim.g.sonokai_style = "maia"
+vim.g.edge_style = "aura"
+return {
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   opts = {
+  --     transparent = true,
+  --     styles = {
+  --       sidebars = "transparent",
+  --       floats = "transparent",
+  --     },
+  --   },
+  -- },
+  { "NTBBloodbath/doom-one.nvim", lazy = true },
+  { "sainnhe/sonokai", lazy = true },
+  { "sainnhe/edge", lazy = true },
+  { "catppuccin/nvim", lazy = true },
+}
