b5127e37b6992dc5f2d0e1649f8ae1b2599b54e2
Tuan Bui <tuanba1@gmail.com>
2023-08-02 17:22:00 +0700

    refactor themes module

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,18 +1,18 @@
 -- vim.g.sonokai_style = "maia"
+-- need
 vim.g.edge_style = "aura"
 return {
-  -- {
-  --   "folke/tokyonight.nvim",
-  --   opts = {
-  --     transparent = true,
-  --     styles = {
-  --       sidebars = "transparent",
-  --       floats = "transparent",
-  --     },
-  --   },
-  -- },
-  { "NTBBloodbath/doom-one.nvim", lazy = true },
-  { "sainnhe/sonokai", lazy = true },
-  { "sainnhe/edge", lazy = true },
-  { "catppuccin/nvim", lazy = true },
+	{ "NTBBloodbath/doom-one.nvim", lazy = true },
+	{ "sainnhe/sonokai", lazy = true },
+	{ "sainnhe/edge", lazy = true },
+	{ "catppuccin/nvim", lazy = true },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			-- colorscheme = "doom-one",
+			-- colorscheme = "sonokai",
+			-- colorscheme = "edge",
+			colorscheme = "catppuccin-macchiato", -- themes
+		},
+	},
 }
