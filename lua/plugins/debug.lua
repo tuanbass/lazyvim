f95728948911b2990a550d297ab44b63cf9e3833
Tuan Bui <tuanba1@gmail.com>
2023-08-02 13:49:37 +0700

    change shortcut for debug and hydra hint position

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,35 +1,46 @@
+local dap = require("dap")
 function debug_hydra()
 	local Hydra = require("hydra")
 	local hint = [[
-	   _b_: Toggle breakpoint ^^^^^^ _r_: Start/Continue ^^^^^^ _o_: Step Over ^^^^^^ _i_: Step Into ^^^^^^ _s_: Stop^^^^^^  _x_: exit
+	   _B_: Toggle breakpoint ^^^^^^   _<F9>_ : Toggle breakpoint ^^^^^^
+     _R_: Start/Continue ^^^^^^      _<F5>_ : Start/Continue ^^^^^^
+     _O_: Step Over ^^^^^^           _<F10>_: Step Over ^^^^^^
+     _I_: Step Into ^^^^^^           _<F11>_: Step Into ^^^^^^
+     _S_: Stop^^^^^^                 _<F12>_: Step Out
+     _d_: Show Hydra^^^^^^          _<F17>_: Run2Cursor(S-F5)
+                     ^^^^^^          _X_    : exit
      ]]
+
 	Hydra({
 		name = "Debug:   ",
 		hint = hint,
 		config = {
 			hint = {
 				type = "window", -- only window works
+				position = "middle-right",
 			},
-			color = "pink",
+			color = "pink", -- Hydra "color" , define type of hydra. Not confuse as the dispaly color
 		},
 		mode = "n",
 		body = "<leader>dd",
 		heads = {
 
 			-- some shortcut doesnot use function key
-			{ "o", function() require("dap").step_over() end, { desc = "Step Over" } }, -- Step Over
-			{ "i", function() require("dap").step_into() end, { desc = "Step Into" } }, -- Step Into
-			{ "b", function() require("dap").toggle_breakpoint() end, { desc = "Toggle breakpoint" } }, -- Toggle breakpoint
-			{ "r", function() require("dap").continue() end, { desc = "Start/Continue" } }, -- start/continue
-			{ "s", function() require("dap").terminate() end, { desc = "Terminate" } }, -- start/continue
-			{ "<F9>", function() require("dap").toggle_breakpoint() end, { desc = "Toggle breakpoint" } }, -- Toggle breakpoint
+			{ "O", function() dap.step_over() end, { desc = "Step Over" } }, -- Step Over
+			{ "I", function() dap.step_into() end, { desc = "Step Into" } }, -- Step Into
+			{ "B", function() dap.toggle_breakpoint() end, { desc = "Toggle breakpoint" } }, -- Toggle breakpoint
+			{ "R", function() dap.continue() end, { desc = "Start/Continue" } }, -- start/continue
+			{ "S", function() dap.terminate() end, { desc = "Terminate" } }, -- start/continue
+			{ "d", function() end, { desc = "Show Hydra" } }, -- start/continue
+			{ "X", nil, { desc = "exit", exit = true } },
+			-- some shortcut use function key
+			{ "<F9>", function() dap.toggle_breakpoint() end, { desc = "Toggle breakpoint" } }, -- Toggle breakpoint
 			-- { "<F21>", "<leader>dB", { desc = "Conditional breakpoint" } }, -- <S-F9>Conditional breakpoint
-			{ "<F5>", function() require("dap").continue() end, { desc = "Start/Continue" } }, -- start/continue
-			{ "<F17>", function() require("dap").run_to_cursor() end, { desc = "Run to Cursor(S-F5)" } }, -- <S-F5>: Run to cursor
-			{ "<F10>", function() require("dap").step_over() end, { desc = "Step Over" } }, -- Step Over
-			{ "<F11>", function() require("dap").step_into() end, { desc = "Step Into" } }, -- Step Into
-			{ "<F12>", function() require("dap").step_out() end, { desc = "Step Out" } }, -- Step Out
-			{ "x", nil, { desc = "exit", exit = true } },
+			{ "<F5>", function() dap.continue() end, { desc = "Start/Continue" } }, -- start/continue
+			{ "<F17>", function() dap.run_to_cursor() end, { desc = "Run to Cursor(S-F5)" } }, -- <S-F5>: Run to cursor
+			{ "<F10>", function() dap.step_over() end, { desc = "Step Over" } }, -- Step Over
+			{ "<F11>", function() dap.step_into() end, { desc = "Step Into" } }, -- Step Into
+			{ "<F12>", function() dap.step_out() end, { desc = "Step Out" } }, -- Step Out
 		},
 	})
 
