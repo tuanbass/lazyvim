f7e46d9a56740b96906e31c82ac5c0070bea9db9
Tuan Bui <tuanba1@gmail.com>
2023-08-30 00:09:19 +0700

    fix the delay with "o" in neotree

--- lua/plugins/explorer.lua
+++ lua/plugins/explorer.lua
@@ -1,10 +1,25 @@
 -- we overwrite behavior of neotree plugin here
 
 local keymap = {
-  ["o"] = {
-    "open",
-    nowait = true, -- disable `nowait` if you have existing combos starting with this char that you want to use
-  },
+  -- ["O"] = { "show_help", nowait = false, config = { title = "Order by 1", prefix_key = "o" } },
+  -- ["Oc"] = { "order_by_created", nowait = false },
+  -- ["Od"] = { "order_by_diagnostics", nowait = false },
+  -- ["Og"] = { "order_by_git_status", nowait = false },
+  -- ["Om"] = { "order_by_modified", nowait = false },
+  -- ["On"] = { "order_by_name", nowait = false },
+  -- ["Os"] = { "order_by_size", nowait = false },
+  -- ["Ot"] = { "order_by_type", nowait = false },
+  -- TODO: tricky, find an elegant way to control.  there is a small delay when press "o"
+  -- tempofix by delete all default mapping for "o<x>" in the neotree plugin
+  -- ~/.local/share/nvim_plugins/lazy-vim/lazy/neo-tree.nvim/lua/neo-tree/defaults.lua
+  ["o"] = { "open", nowait = true, config = { prefix_key = "" } },
+  ["oc"] = "noop",
+  ["od"] = "noop",
+  ["og"] = "noop",
+  ["om"] = "noop",
+  ["on"] = "noop",
+  ["os"] = "noop",
+  ["ot"] = "noop",
   ["<Tab>"] = { "toggle_preview", config = { use_float = false } },
 
   -- copy relative path
