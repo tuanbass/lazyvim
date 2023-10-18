06f55dc83aa95ba24538d3caaebbded387c2443b
Tuan Bui <tuanba1@gmail.com>
2023-10-18 11:00:37 +0700

    style(scope): format code

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,43 +1,42 @@
 local session_path = vim.fn.stdpath("data") .. "/sessions/"
 
 return {
-	{
-		"JoseConseco/telescope_sessions_picker.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", "echasnovski/mini.sessions" },
-		config = function(plugin)
-			require("mini.sessions").setup({
-				autoread = true,
-				autowrite = true,
-				-- file = "Session.vim",
-				directory = session_path,
-			})
-			require("telescope").setup({
-				-- your existing tele config
-				extensions = {
-					sessions_picker = {
-						sessions_dir = session_path,
-					},
-					-- other tele extensions configs
-				},
-			})
-			require("telescope").load_extension("sessions_picker")
+  {
+    "JoseConseco/telescope_sessions_picker.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "echasnovski/mini.sessions" },
+    config = function(plugin)
+      require("mini.sessions").setup({
+        autoread = true,
+        autowrite = true,
+        -- file = "Session.vim",
+        directory = session_path,
+      })
+      require("telescope").setup({
+        -- your existing tele config
+        extensions = {
+          sessions_picker = {
+            sessions_dir = session_path,
+          },
+          -- other tele extensions configs
+        },
+      })
+      require("telescope").load_extension("sessions_picker")
 
-			-- create some custom commands
-			require("utils.cmd").createCommand(plugin.cmds or {})
-		end,
-		keys = { { "<leader>fs", "<cmd>Telescope sessions_picker<cr>", desc = "Sessions" } },
+      -- create some custom commands
+      require("utils.cmd").createCommand(plugin.cmds or {})
+    end,
+    keys = { { "<leader>fs", "<cmd>Telescope sessions_picker<cr>", desc = "Sessions" } },
 
-		cmds = {
-			{
-				"MkSession", -- :MkSession <session-name> , to create a session. Buffers in this session will be auto tracked from now on
-				function(opts)
-					local sess_name = opts.args
-					if sess_name == nil or sess_name == "" then sess_name = "Session.vim" end
-					print("sessions name: ", sess_name)
-					require("mini.sessions").write(sess_name)
-				end,
-				{ nargs = "?" }, -- 0 or 1 args
-			},
-		},
-	},
+    cmds = {
+      {
+        "MkSession", -- :MkSession <session-name> , to create a session. Buffers in this session will be auto tracked from now on
+        function(opts)
+          local sess_name = opts.args
+          if sess_name == nil or sess_name == "" then sess_name = "Session.vim" end
+          require("mini.sessions").write(sess_name)
+        end,
+        { nargs = "?" }, -- 0 or 1 args
+      },
+    },
+  },
 }
--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -2,17 +2,17 @@
 -- need
 vim.g.edge_style = "aura"
 return {
-	{ "NTBBloodbath/doom-one.nvim", lazy = true },
-	{ "sainnhe/sonokai", lazy = true },
-	{ "sainnhe/edge", lazy = true },
-	{ "catppuccin/nvim", lazy = true },
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			-- colorscheme = "doom-one",
-			-- colorscheme = "sonokai",
-			-- colorscheme = "edge",
-			colorscheme = "catppuccin-macchiato", -- themes
-		},
-	},
+  { "NTBBloodbath/doom-one.nvim", lazy = true },
+  { "sainnhe/sonokai", lazy = true },
+  { "sainnhe/edge", lazy = true },
+  { "catppuccin/nvim", lazy = true },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "doom-one",
+      -- colorscheme = "sonokai",
+      colorscheme = "edge",
+      -- colorscheme = "catppuccin-macchiato", -- themes
+    },
+  },
 }
