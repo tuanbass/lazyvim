9f791ff2c3b57b34d1080742c79ed7236a6ddb00
Tuan Bui <tuanba1@gmail.com>
2023-12-10 13:41:25 +0700

    change default session path/name when creating new session

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -4,6 +4,7 @@ return {
   {
     "JoseConseco/telescope_sessions_picker.nvim",
     dependencies = { "nvim-telescope/telescope.nvim", "echasnovski/mini.sessions" },
+    lazy = false,
     config = function(plugin)
       require("mini.sessions").setup({
         autoread = true,
@@ -25,18 +26,39 @@ return {
       -- create some custom commands
       require("utils.cmd").createCommand(plugin.cmds or {})
     end,
-    keys = { { "<leader>fs", "<cmd>Telescope sessions_picker<cr>", desc = "Sessions" } },
+    keys = {
+      { "<leader>S", "", desc = "Sessions" },
+      { "<leader>Sl", "<cmd>Telescope sessions_picker<cr>", desc = "Sessions load" },
+      { "<leader>Ss", "<cmd>SessionSave<cr>", desc = "Sessions create" },
+    },
 
     cmds = {
       {
-        "MkSession", -- :MkSession <session-name> , to create a session. Buffers in this session will be auto tracked from now on
+        "SessionSave", -- :MkSession <session-name> , to create a session. Buffers in this session will be auto tracked from now on
         function(opts)
           local sess_name = opts.args
-          if sess_name == nil or sess_name == "" then sess_name = "Session.vim" end
+          if sess_name == nil or sess_name == "" then
+            local current_folder = vim.fn.getcwd()
+            sess_name = vim.fn.fnamemodify(current_folder, ":t")
+          end
           require("mini.sessions").write(sess_name)
         end,
         { nargs = "?" }, -- 0 or 1 args
       },
     },
   },
+  -- yes, another session manager
+  -- {
+  --   "mrquantumcodes/retrospect.nvim",
+  --   lazy = false,
+  --   config = function()
+  --     local retrospect = require("retrospect")
+  --     -- retrospect.setup({
+  --     --   saveKey = "<leader>\\", -- The shortcut to save the session, default is leader+backslash(\)
+  --     --   loadKey = "<leader><BS>", -- The shortcut to load the session
+  --     --   style = "default", -- or "modern", if you have nui.nvim and dressing.nvim
+  --     -- })
+  --   end,
+  --   -- cmd = { "SessionLoad", function() require("retrospect").RestoreSession() end, desc = "Load Session" },
+  -- },
 }
